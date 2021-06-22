57ace10e92b7cc1dd54a48c38bbbb77f308d5c9b
Emmanuel <63491451+emmyblessing@users.noreply.github.com>
2021-06-22 14:16:57 +0100

    print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * print_alphabet - check the code for Holberton School students.
+ * print_alphabet_x10 - func
  *
- * Return: Always 0.
+ * Return - 0
  */
 void print_alphabet_x10(void)
 {
