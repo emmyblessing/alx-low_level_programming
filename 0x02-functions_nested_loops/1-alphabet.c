d95fa9d0228c6cdc78648ff57513f6990f2d7ed5
emmyblessing <emmyblessing78@gmail.com>
2021-06-22 05:23:20 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#rint_alphabet - check the code for Holberton School students.
+~ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char alp = 'a';
+
+while (alp <= 'z')
+{
+_putchar (alp);
+alp++;
+}
+ 
+_putchar ('\n');
+
+}
