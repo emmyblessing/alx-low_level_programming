0461e5d4f5f1db96735722a92fb012f018654661
emmyblessing <emmyblessing78@gmail.com>
2021-07-01 06:57:02 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * reverse_array - reverse a int array
+ * @a: pointer
+ * @n: array length
+ * Return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+int i;
+int j;
+int tmp;
+
+i = 0;
+j = n - 1;
+
+while (i < j)
+{
+tmp = a[i];
+a[i] = a[j];
+a[j] = tmp;
+i++;
+j--;
+}
+}
